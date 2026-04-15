611981a3f687ce7c1d546e910f23336a5fa7aa7a
goncalolourenco09 <goncalolourenco.14376@ae-danielsampaio.pt>
2026-04-15 10:34:43 +0100

    jogador.py

--- src/jogador.py
+++ src/jogador.py
@@ -89,38 +89,3 @@ def remover_jogador_service(id_jogador):
             return 200, j
 
     return 404, "Jogador não encontrado"
-
-
-clubes = []
-ultimo_id_clube = 0
-
-
-def gerar_id_clube():
-    global ultimo_id_clube
-    ultimo_id_clube += 1
-    return ultimo_id_clube
-
-
-def criar_clube_service(nome, nif):
-    # verificar se já existe NIF
-    for c in clubes:
-        if c["nif"] == nif:
-            return 409, "Clube com este NIF já existe!"
-
-    clube = {
-        "id_clube": gerar_id_clube(),
-        "nome": nome,
-        "nif": nif
-    }
-
-    clubes.append(clube)
-    return 200, clube
-
-
-def remover_clube_service(id_clube):
-    for c in clubes:
-        if c["id_clube"] == id_clube:
-            clubes.remove(c)
-            return 200, c
-
-    return 404, "Clube não encontrado"

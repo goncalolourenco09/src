74535735e7bd017c65b517380326725f39f040f6
apito <goncalolourenco.14376@ae-danielsampaio.pt>
2026-05-13 10:53:19 +0100

    clube.py

--- src/clube.py
+++ src/clube.py
@@ -1,12 +1,25 @@
 from utils import gerar_id_clube, validar_nome, validar_nif
+from persistencia import guardar, carregar, FICHEIRO_CLUBES
 
 clubes = {}
+
+# ==========================
+# Persistência
+# ==========================
+
+def guardar_clubes():
+    guardar(FICHEIRO_CLUBES, clubes)
+
+def carregar_clubes():
+    global clubes
+    clubes = carregar(FICHEIRO_CLUBES)
 
 # ==========================
 # CREATE
 # ==========================
 
 def criar_clube(nome, nif):
+    carregar_clubes()
     if not validar_nome(nome):
         return 500, "Nome inválido."
     if not validar_nif(nif):
@@ -20,6 +33,7 @@ def criar_clube(nome, nif):
         "nif": nif
     }
     clubes[id_clube] = clube
+    guardar_clubes()
     return 201, clube
 
 # ==========================
@@ -27,6 +41,7 @@ def criar_clube(nome, nif):
 # ==========================
 
 def listar_clubes():
+    carregar_clubes()
     if not clubes:
         return 404, "Não existem clubes registados."
     return 200, clubes
@@ -36,6 +51,7 @@ def listar_clubes():
 # ==========================
 
 def consultar_clube(id_clube):
+    carregar_clubes()
     if id_clube not in clubes:
         return 404, "Clube não encontrado."
     return 200, clubes[id_clube]
@@ -45,6 +61,7 @@ def consultar_clube(id_clube):
 # ==========================
 
 def atualizar_clube(id_clube, nome=None, nif=None):
+    carregar_clubes()
     if id_clube not in clubes:
         return 404, "Clube não encontrado."
     if nome:
@@ -58,6 +75,7 @@ def atualizar_clube(id_clube, nome=None, nif=None):
             if c["nif"] == nif and id_c != id_clube:
                 return 409, "Já existe um clube com este NIF."
         clubes[id_clube]["nif"] = nif
+    guardar_clubes()
     return 200, clubes[id_clube]
 
 # ==========================
@@ -65,7 +83,9 @@ def atualizar_clube(id_clube, nome=None, nif=None):
 # ==========================
 
 def remover_clube(id_clube):
+    carregar_clubes()
     if id_clube not in clubes:
         return 404, "Clube não encontrado."
     del clubes[id_clube]
+    guardar_clubes()
     return 200, id_clube
